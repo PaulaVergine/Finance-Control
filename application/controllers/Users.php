2a4092e1ca92c732d73dbfdbcc442bc30aff0f1c
Julio C. Martins <jcmartins87@outlook.com>
2016-09-24 15:36:35 -0300

    users controller, ajustes e correcoes new_user

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -4,6 +4,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Users extends CI_Controller {
 
     public function newUser(){
-        $this->load->view("users/new_user.php");
+
+        
+
+        if($this->input->post('submitEmail')){
+            $email = $this->input->post('email');
+            
+            $parametros = array('email' => $email);
+            
+            $this->load->view("users/new_user.php",$parametros);
+            
+        }elseif ($this->input->post('submitUser')){
+            $this->load->view("users/new_user_conf.php");
+        }else{
+            $this->load->view("users/new_user.php");
+        }
+        
     }
 }
